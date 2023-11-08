5eed3920c8c7d059576d04e3c6e42c96e4ee541c
huseyn-eziz <mastereziz@gmail.com>
2023-11-08 23:25:32 +0400

    interno 2 finished

--- src/Components/WorkPlanCards/index.jsx
+++ src/Components/WorkPlanCards/index.jsx
@@ -1,39 +1,43 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "./style.module.css";
+import { Link } from "react-router-dom";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
-
-const url = 'http://localhost:3000/WorkPlan'
-
+const _url = "http://localhost:3000/WorkPlan";
 const WorkPlanCards = () => {
-
-  const [cards, setCards] = useState([])
-
+  const [cards, setCards] = useState([]);
   useEffect(() => {
-    axios.get(url).then(({data})=>{
-      setCards(data)
-    })
-  }, [])
-  
-
+    axios.get(_url).then(({ data }) => {
+      setCards(data);
+    });
+  }, []);
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-4">
-          {cards.map(({id,title,description})=> {
-            return (
-              <div
+      <div className="row mb-5 mt-2">
+        {cards.map(({ id, title, description }) => {
+          return (
+            <div
               className={`col-12 col-xl-4 my-3 ${styles.WorkPlanCard}`}
               key={id}
             >
               <h3 className={`my-3 ${styles.title}`}>{title}</h3>
               <p>{description}</p>
+              <Link
+                className={styles.readMoreButton}
+                to={`/WorkPlanCardDetail/${id}`}
+              >
+                Read More
+                <span>
+                  <AiOutlineArrowRight />
+                </span>
+              </Link>
             </div>
-            )
-          })}
-        </div>
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WorkPlanCards
+export default WorkPlanCards;
